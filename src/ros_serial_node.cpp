c504a0b71ebf0d956d6a96ff567976572da05e98
sean <sgilbert.glitches@gmail.com>
2023-04-01 11:46:07 -0700

    added serial library from wjwwood for basic serial function

--- src/ros_serial_node.cpp
+++ src/ros_serial_node.cpp
@@ -1,79 +1,88 @@
-#include <rclcpp/rclcpp.hpp>
-#include <rclcpp_serial/serial_port.hpp>
-#include <std_msgs/msg/string.hpp>
+#include <iostream>
+#include <memory>
+#include <string>
 
-class SerialPortNode : public rclcpp::Node
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "serial/serial.h"
+
+class SerialNode : public rclcpp::Node
 {
 public:
-  SerialPortNode()
-  : Node("serial_port_node")
-  {
-    // Create a SerialPort object
-    std::string port_name = "/dev/ttyUSB0";
-    int baud_rate = 115200;
-    serial_port_ = std::make_unique<rclcpp_serial::SerialPort>(port_name, baud_rate);
+    SerialNode()
+        : Node("serial_node"), port_name_("/dev/ttyUSB0"), baud_rate_(115200)
+    {
+        publisher_ = this->create_publisher<std_msgs::msg::String>("serial_data", 10);
+        subscriber_ = this->create_subscription<std_msgs::msg::String>(
+            "serial_send", 10, std::bind(&SerialNode::serial_send_callback, this, std::placeholders::_1));
 
-    // Open the serial port
-    if (!serial_port_->open()) {
-      RCLCPP_ERROR(get_logger(), "Failed to open serial port");
-      return;
+        try
+        {
+            serial_.setPort(port_name_);
+            serial_.setBaudrate(baud_rate_);
+            serial::Timeout timeout = serial::Timeout::simpleTimeout(1000);
+            serial_.setTimeout(timeout);
+            serial_.open();
+        }
+        catch (const serial::IOException &e)
+        {
+            RCLCPP_ERROR(this->get_logger(), "Unable to open port %s", port_name_.c_str());
+        }
+
+        if (serial_.isOpen())
+        {
+            RCLCPP_INFO(this->get_logger(), "Successfully opened port %s", port_name_.c_str());
+            serial_timer_ = this->create_wall_timer(std::chrono::milliseconds(100),
+                                                    std::bind(&SerialNode::serial_read_callback, this));
+        }
     }
-
-    // Create a subscriber to receive messages from ROS
-    subscriber_ = create_subscription<std_msgs::msg::String>(
-      "serial_tx", 10, std::bind(&SerialPortNode::onSerialTx, this, std::placeholders::_1));
-
-    // Create a publisher to send messages to ROS
-    publisher_ = create_publisher<std_msgs::msg::String>("serial_rx", 10);
-
-    // Create a timer to read data from the serial port
-    timer_ = create_wall_timer(std::chrono::milliseconds(10), std::bind(&SerialPortNode::onTimer, this));
-  }
 
 private:
-  void onSerialTx(const std_msgs::msg::String::SharedPtr msg)
-  {
-    // Send the message to the serial port
-    std::vector<uint8_t> buffer(msg->data.begin(), msg->data.end());
-    serial_port_->write(buffer);
-  }
-
-  void onTimer()
-  {
-    // Read data from the serial port
-    std::vector<uint8_t> buffer;
-    size_t num_bytes = serial_port_->read(buffer);
-
-    // Process the received data
-    if (num_bytes > 0) {
-      // Convert the buffer to a string
-      std::string message(buffer.begin(), buffer.end());
-
-      // Print the received message
-      RCLCPP_INFO(get_logger(), "Received message: %s", message.c_str());
-
-      // Publish the message to ROS
-      auto msg = std_msgs::msg::String();
-      msg.data = message;
-      publisher_->publish(msg);
+    void serial_send_callback(const std_msgs::msg::String::SharedPtr msg)
+    {
+        if (serial_.isOpen())
+        {
+            try
+            {
+                serial_.write(msg->data);
+            }
+            catch (const serial::IOException &e)
+            {
+                RCLCPP_ERROR(this->get_logger(), "Error writing to the serial port");
+            }
+        }
     }
-  }
 
-  std::unique_ptr<rclcpp_serial::SerialPort> serial_port_;
-  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscriber_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-  rclcpp::TimerBase::SharedPtr timer_;
+    void serial_read_callback()
+    {
+        if (serial_.available())
+        {
+            try
+            {
+                std::string data = serial_.readline();
+                auto message = std_msgs::msg::String();
+                message.data = data;
+                publisher_->publish(message);
+            }
+            catch (const serial::IOException &e)
+            {
+                RCLCPP_ERROR(this->get_logger(), "Error reading from the serial port");
+            }
+        }
+    }
+
+    std::string port_name_;
+    int baud_rate_;
+    serial::Serial serial_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscriber_;
+    rclcpp::TimerBase::SharedPtr serial_timer_;
 };
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-  rclcpp::init(argc, argv);
-
-  auto node = std::make_shared<SerialPortNode>();
-
-  rclcpp::spin(node);
-
-  rclcpp::shutdown();
-
-  return 0;
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<SerialNode>());
+    rclcpp::shutdown();
+    return 0;
 }
